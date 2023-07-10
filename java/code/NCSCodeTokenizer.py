a85ba23e9c2c040845044b5c88daf6824dbd5d92
Georgy Andryushchenko <georgyandryuschenko@gmail.com>
2023-07-10 12:50:23 +0300

    java code tokenization

---
+++ java/code/NCSCodeTokenizer.py
@@ -0,0 +1,11 @@
+from code_tokenizer import CodeTokenizer
+from simple_tokenizer import SimpleTokenizer
+
+class NCSCodeTokenizer:
+    def __init__(self):
+        self.simple_tokenizer = SimpleTokenizer()
+        self.code_tokenizer = CodeTokenizer()
+    def tokenize(self, code):
+        tokens = [x[0] for x in self.simple_tokenizer.tokenize(code).data]
+        tokens = [x[0] for x in self.code_tokenizer.tokenize(" ".join(tokens)).data]
+        return tokens
